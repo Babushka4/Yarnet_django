118378cddd6d7607657d8c747a320d7b262e8186
Crinax <agreedeth@gmail.com>
2022-02-13 09:46:41 +0000

    Change HTTPResponse from plain/text to application/json

--- yarnetbpm/employees/views.py
+++ yarnetbpm/employees/views.py
@@ -1,33 +1,35 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+import json
+from django.http import JsonResponse, HttpResponse
 from django.core import serializers
 
 from employees.models import Employee
-from random import randint
 
 def index(request):
   params = dict(request.GET)
+  print(params)
   if (len(params) == 5):
     if ('fullname' not in params):
-      return HttpResponse('{ "error": "Fullname not specified" }')
+      return JsonResponse({ "error": "Fullname not specified" })
     if ('department' not in params):
-      return HttpResponse('{ "error": "Department not specified" }')
+      return JsonResponse({ "error": "Department not specified" })
     if ('position' not in params):
-      return HttpResponse('{ "error": "Position not specified" }')
+      return JsonResponse({ "error": "Position not specified" })
     if ('email' not in params):
-      return HttpResponse('{ "error": "Email not specified" }')
+      return JsonResponse({ "error": "Email not specified" })
     if ('telephone' not in params):
-      return HttpResponse('{ "error": "Telephone not specified" }')
+      return JsonResponse({ "error": "Telephone not specified" })
 
     new_empl = Employee(
-      fullname=params['fullname'],
-      department=params['department'],
-      position=params['position'],
-      email=params['email'],
-      telephone=params['telephone'],
+      fullname=params['fullname'][0],
+      department=params['department'][0],
+      position=params['position'][0],
+      email=params['email'][0],
+      telephone=params['telephone'][0],
     )
     new_empl.save()
-
-  return HttpResponse(serializers.serialize('json', Employee.objects.all()))
+  
+  response = serializers.serialize('json', Employee.objects.all())
+  return HttpResponse(response, content_type="application/json")
 
 # Create your views here.
