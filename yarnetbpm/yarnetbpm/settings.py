0709368df0efa65376d1dee84ca9d8dbcadd5dd8
Crinax <agreedeth@gmail.com>
2022-02-13 15:43:33 +0000

    Added API application and temporarily disabled CSRF

--- yarnetbpm/yarnetbpm/settings.py
+++ yarnetbpm/yarnetbpm/settings.py
@@ -62,6 +62,7 @@ SECRET_KEY = get_var('SECRET_KEY')
 
 INSTALLED_APPS = [
     'employees.apps.EmployeesConfig',
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,7 +75,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
