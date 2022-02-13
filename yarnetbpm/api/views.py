f03f18bc871a3b2951759e4a3b9d12add2950c77
Crinax <agreedeth@gmail.com>
2022-02-13 15:50:27 +0000

    Added logic for `v1/employees/*`

--- yarnetbpm/api/views.py
+++ yarnetbpm/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
 
-# Create your views here.
+from employees.models import Employee
+from decorators import GET, POST, deserialize_body, body_has
+
+@GET
+def get_employees(request):
+  response = serializers.serialize('json', Employee.objects.all())
+
+  return HttpResponse(response, content_type="application/json")
+
+@POST
+@deserialize_body
+@body_has([
+  'fullname',
+  'department',
+  'position',
+  'email',
+  'telephone'
+])
+def add_employees(request):
+  body = request.deserialized
+
+  try:
+    new_empl = Employee(
+      fullname=body['fullname'],
+      department=body['department'],
+      position=body['position'],
+      email=body['email'],
+      telephone=body['telephone'],
+    )
+
+    new_empl.save()
+
+    return JsonResponse({ 'ok': True })
+  except:
+    return HttpResponse('Internal Server Error', status=500)
