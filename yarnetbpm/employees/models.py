a8390f7a360bbcc82591c012dc05d40107014b2d
Crinax <agreedeth@gmail.com>
2022-02-14 14:43:20 +0000

    Changed CharField to ForeignKey in department field

--- yarnetbpm/employees/models.py
+++ yarnetbpm/employees/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
+from department.models import Department
+
 class Employee(models.Model):
     fullname = models.CharField(max_length=255)
-    department = models.CharField(max_length=255)
+    department = models.ForeignKey(Department, on_delete=models.PROTECT)
     position = models.CharField(max_length=100)
     email = models.CharField(max_length=255)
     telephone = models.CharField(max_length=15)
-
-
