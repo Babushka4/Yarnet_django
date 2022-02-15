237d9265ad3764738b8e07bb2fa3fd383a088a60
Crinax <agreedeth@gmail.com>
2022-02-15 15:03:36 +0000

    Added string view of model

--- yarnetbpm/department/models.py
+++ yarnetbpm/department/models.py
@@ -5,3 +5,6 @@ from organization.models import Organization
 class Department(models.Model):
   name = models.CharField(max_length=255)
   organizations = models.ManyToManyField(Organization)
+
+  def __str__(self):
+        return f"{self.name} [{self.id}]"
