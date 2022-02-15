763f7397da48ab96f5ed2281ed974d0dfc50f660
Crinax <agreedeth@gmail.com>
2022-02-15 11:54:37 +0000

    Added routes for departments and organizations

--- yarnetbpm/api/urls.py
+++ yarnetbpm/api/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 urlpatterns = [
     path('v1/employees/', views.get_employees, name='get_employees'),
-    path('v1/employees/add/', views.add_employees, name='add_employees')
+    path('v1/employees/add/', views.add_employees, name='add_employees'),
+
+    path('v1/departments/', views.get_departments, name='get_departments'),
+    path('v1/departments/add/', views.add_departments, name='add_departments'),
+
+    path('v1/organizations/', views.get_organizations, name='get_organizations'),
+    path('v1/organizations/add/', views.add_organizations, name='add_organizations')
 ]
