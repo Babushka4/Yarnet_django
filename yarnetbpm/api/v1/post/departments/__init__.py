e004531b6e000f18805ac08c958d48be6c55978e
Crinax <agreedeth@gmail.com>
2022-02-15 15:03:02 +0000

    Fixed adding organization

--- yarnetbpm/api/v1/post/departments/__init__.py
+++ yarnetbpm/api/v1/post/departments/__init__.py
@@ -18,17 +18,18 @@ def add(request):
       organizations = Organization.objects.filter(pk__in=body['organizations'])
 
       if (len(organizations) != 0):
-        new_department = Department(
-          name=body['name'],
-          organizations=organizations
-        )
+        new_department = Department(name=body['name'])
 
+        new_department.save()
+        new_department.organizations.add(*organizations)
         new_department.save()
 
         return JsonResponse({ 'ok': True })
       else:
         return JsonResponse({ 'ok': False, 'message': 'No such organizations' }, status=400)
-    except:
+    except Exception as e:
+      e.with_traceback()
       return HttpResponse('Internal Server Error', status=500)
-  except:
+  except Exception as e:
+    e.with_traceback()
     return HttpResponse('Internal Server Error', status=500)
