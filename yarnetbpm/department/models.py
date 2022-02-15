50bf3c9b8a0bb9ee9179b02285a10b9acd2642f4
Crinax <agreedeth@gmail.com>
2022-02-15 11:27:38 +0000

    Linked Department with Organization

--- yarnetbpm/department/models.py
+++ yarnetbpm/department/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from organization.models import Organization
+
 class Department(models.Model):
   name = models.CharField(max_length=255)
-  organization = models.CharField(max_length=255)
+  organizations = models.ManyToManyField(Organization)
