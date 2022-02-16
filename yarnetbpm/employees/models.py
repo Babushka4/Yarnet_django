ba07de3ad71dc275d39b5da38b064eb9a8e4a010
Crinax <agreedeth@gmail.com>
2022-02-16 15:13:00 +0000

    Removed unnecessary method

--- yarnetbpm/employees/models.py
+++ yarnetbpm/employees/models.py
@@ -13,6 +13,3 @@ class Employee(models.Model):
 
     def __str__(self):
         return f"{self.email} - {self.fullname} [{self.position} -> {self.department}] ({self.id})"
-
-    def get_department_name(self):
-        return Department.objects.filter(pk=self.department_id)[0].name
