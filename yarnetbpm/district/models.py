40516f16f8b3029da4f83a62d382c1780539a30e
Crinax <agreedeth@gmail.com>
2022-02-23 15:25:37 +0000

    Added string value for model

--- yarnetbpm/district/models.py
+++ yarnetbpm/district/models.py
@@ -4,3 +4,6 @@ from django.db import models
 
 class District(models.Model):
   name = models.CharField(max_length=255)
+
+  def __str__(self):
+      return f"{self.name} [{self.id}]"
--- yarnetbpm/violation/models.py
+++ yarnetbpm/violation/models.py
@@ -5,4 +5,7 @@ from vsubspecies.models import VSubspecies
 
 class Violation(models.Model):
   name = models.CharField(max_length=255)
-  subtype = models.ForeignKey(VSubspecies, on_delete=models.PROTECT)
+  subspecies = models.ForeignKey(VSubspecies, on_delete=models.PROTECT, default=None)
+
+  def __str__(self):
+      return f"{self.name}"
