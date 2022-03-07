296317ffc07a044c38ee5cb0eb7558a75426abec
Crinax <agreedeth@gmail.com>
2022-03-07 10:31:34 +0000

    Added method to get form and create logic for task creator

--- yarnetbpm/task/views.py
+++ yarnetbpm/task/views.py
@@ -1,12 +1,17 @@
+import json
+
+from datetime import datetime
 from django.views.generic import TemplateView
+from django.template import loader
+from django.http import HttpResponse
 from django.shortcuts import redirect
 
-from task.models import Task, Fields
+from task.models import Task, Fields, Values
 from regulations.models import Regulations
 from company.models import Company
 from violation.models import Violation
 from user.models import User
-from datetime import datetime
+from decorators import POST
 
 class TaskInfo(TemplateView):
   template_name = 'task_info.html'
@@ -28,10 +33,10 @@ class TaskTable(TemplateView):
     _id = int(request.POST.get('next_id'))
     _task_id = int(request.POST.get('task_id'))
     [_task] = Task.objects.filter(pk=_task_id)
-    [_regulations] = Regulations.objects.filter(pk=_id)
-    _task.regulations = _regulations
+    [_stage] = Regulations.Stage.objects.filter(pk=_id)
+    _task.stage = _stage
     
-    if len(_task.regulations.childs) == 0:
+    if len(_task.stage.childs) == 0:
       _task.is_completed = True
 
     _task.save()
@@ -44,38 +49,74 @@ class TaskTable(TemplateView):
     context['districts'] = Task.District.choices
     context['violation_list'] = Violation.objects.all()
     context['user_list'] = User.objects.all()
-    context['new_task_list'] = Task.objects.filter(is_completed=False)
-    context['reg_types'] = Regulations.Types
+    context['stage_schemes'] = Regulations.Stage.Types
     context['field_types'] = Fields.Types
+    context['regulations_list'] = Regulations.objects.all()
 
     if 'id' in kwargs:
-      context['task_list'] = self.model.objects.filter(user=kwargs['id'])
+      context['task_list'] = self.model.objects.filter(user=kwargs['id'], is_completed=False)
     else:
-      context['task_list'] = self.model.objects.all()
+      context['task_list'] = self.model.objects.filter(is_completed=False)
+
+    print(context['task_list'])
 
     return context
 
 # Добавление новой задачи
 class AddNewTask(TemplateView):
-  model = Task
 
   def post(self, request, *args, **kwargs):
-    user_id = int(request.POST.get('user'))
-    company_id = int(request.POST.get('company'))
-    district = request.POST.get('district')
-    number = request.POST.get('number')
-    user = User.objects.filter(pk=user_id)[0]
-    company = Company.objects.filter(pk=company_id)[0]
-    regulation = Regulations.objects.filter(parent=None)[0]
+    task_name = request.POST.get('task_name')
+    reg_id = int(request.POST.get('reg_id'))
+    task_performer = int(request.POST.get('task_performer'))
+    [regulations] = Regulations.objects.filter(pk=reg_id)
+    [task_performer] = User.objects.filter(pk=task_performer)
+    [author, *_] = User.objects.all()
+    values = []
+    fields = regulations.first_stage.fields_set.all()
+    _class = {
+      Fields.Types.USER: User,
+      Fields.Types.COMPANY: Company,
+    }
+    
+    for field in fields:
+      posted_value = request.POST.get(f"value_{field.id}")
+      correct_value = posted_value
+
+      if field.field_type == Fields.Types.DATE:
+        correct_value = datetime.strptime(posted_value, '%H:%M %m.%d.%Y')
+
+      if field.field_type in [Fields.Types.USER, Fields.Types.COMPANY]:
+        correct_value = int(correct_value)
+        [correct_value] = _class[field.field_type].objects.filter(pk=correct_value)
+
+      new_value = Values(field=field, value=correct_value)
+      new_value.save()
+      values.append(new_value)
+    
     new_task = Task(
-      company=company,
-      user=user,
-      district=district,
-      number=number,
-      regulation=regulation,
-      date=datetime.now(),
+      name=task_name,
+      regulations=regulations,
+      stage=regulations.first_stage,
+      author=author,
+      performer=task_performer,
     )
 
     new_task.save()
 
     return redirect('/tasks/')
+
+
+@POST
+def get_form(request):
+  regulations_id = request.POST.get('id')
+  t = loader.get_template('create_new_task_content.html')
+  render_data = {
+    'regulations': Regulations.objects.filter(pk=regulations_id)[0],
+    'field_types': Fields.Types,
+    'all_users': User.objects.all(),
+    'all_companies': Company.objects.all(),
+  }
+  html = t.render(render_data, request=request)
+
+  return HttpResponse(json.dumps({'html': html}), 'application/json')
