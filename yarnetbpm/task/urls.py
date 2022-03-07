43a046be9493a1c390540091be4b7cb1c159b4ef
Crinax <agreedeth@gmail.com>
2022-03-07 10:31:01 +0000

    Added path to get sidebar form

--- yarnetbpm/task/urls.py
+++ yarnetbpm/task/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from task.views import TaskInfo, TaskTable, AddNewTask
+from task.views import TaskInfo, TaskTable, AddNewTask, get_form
 
 urlpatterns = [
     path('', TaskTable.as_view()),
     path('<int:id>', TaskTable.as_view()),
     path('new_task/', AddNewTask.as_view()),
-    path('task_info/', TaskInfo.as_view())
+    path('task_info/', TaskInfo.as_view()),
+    path('get-form/', get_form)
 ]
