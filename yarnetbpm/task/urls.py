3118e4eeaf6273dd79cc070cccc641f01b067400
Crinax <agreedeth@gmail.com>
2022-03-08 08:37:55 +0000

    Added path to get sidebar body

--- yarnetbpm/task/urls.py
+++ yarnetbpm/task/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from task.views import TaskInfo, TaskTable, AddNewTask, get_form
+from task.views import TaskInfo, TaskTable, AddNewTask, get_form, get_sidebar_body
 
 urlpatterns = [
     path('', TaskTable.as_view()),
     path('<int:id>', TaskTable.as_view()),
     path('new_task/', AddNewTask.as_view()),
     path('task_info/', TaskInfo.as_view()),
-    path('get-form/', get_form)
+    path('get-form/', get_form),
+    path('get-sidebar-body/', get_sidebar_body)
 ]
