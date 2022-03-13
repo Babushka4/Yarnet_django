b5a350b6265f2c1b811d6b2597a944ff47d5adf4
Crinax <agreedeth@gmail.com>
2022-03-13 09:51:17 +0000

    Added registration page

--- yarnetbpm/authentication/urls.py
+++ yarnetbpm/authentication/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from .views import LoginView, RegisterView
+
 urlpatterns = [
-    
+  path('login', LoginView.as_view()),
+  path('registration', RegisterView.as_view()),
 ]
--- yarnetbpm/authentication/views.py
+++ yarnetbpm/authentication/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic import TemplateView
 
 from user.models import User
@@ -7,9 +7,45 @@ from user.models import User
 class LoginView(TemplateView):
   template_name = 'login.html'
 
+  def get_context_data(self, **kwargs):
+      return super().get_context_data(**kwargs)
+
   def post(self, request):
     user = self.authenticate(request.POST.get('username'), request.POST.get('password'))
 
   def authenticate(self, username, password):
     user = User.objects.filter(username=username)
     
+class RegisterView(TemplateView):
+  template_name = 'registration.html'
+
+  def get(self, request, *args, **kwargs):
+    if not request.user.is_anonymous:
+      return redirect('/')
+    
+    return render(request, 'registration.html')
+
+  def post(self, request, *args, **kwargs):
+    login = request.POST.get('login')
+    user = User.objects.filter(username=login)
+
+    if user:
+      return render(
+        request,
+        'registration.html',
+        { "error_message": "Такой пользователь уже существует." }
+      )
+    
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    telephone = request.POST.get('telephone')
+    fullname = request.POST.get('fullname')
+    user = User.objects.create_user(login, password)
+    user.telephone = telephone
+    user.email = email
+    user.fullname = fullname
+
+    user.save()
+
+    return redirect('/')
+
