a803274c18184d2814beb922c8bb90bc6896ed3e
Crinax <agreedeth@gmail.com>
2022-03-13 08:06:07 +0000

    Added home page

--- yarnetbpm/yarnetbpm/urls.py
+++ yarnetbpm/yarnetbpm/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', include('authentication.urls')),
+    path('auth/', include('authentication.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('users/', include('user.urls')),
     path('tasks/', include('task.urls')),
-    path('ltr/', include('ltr.urls'))
+    path('ltr/', include('ltr.urls')),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
